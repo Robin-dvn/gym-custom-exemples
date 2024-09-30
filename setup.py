d07b31e1df59cf7bf0f1e439fc73f0d6b4a6e738
Robin Davenne <robin.davenne@grenoble-inp.org>
2024-09-30 16:26:59 +0900

    ajout de la nouvelle version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="gym_custom",
-    version="0.0.1",
+    version="0.0.2",
     packages=['gym_custom', 'gym_custom.envs'],
     install_requires=["gymnasium", "pygame"],
 )
