4c5c83f68459b7448a03902614caf5e26cc3deb4
Robin Davenne <robin.davenne@grenoble-inp.org>
2024-10-01 23:19:17 +0900

    ajout du random walk de 19 états intermédiaires

--- gym_custom/__init__.py
+++ gym_custom/__init__.py
@@ -5,3 +5,8 @@ register(
      entry_point="gym_custom.envs:GymWalk",
      max_episode_steps=300,
 )
+register(
+     id="GymWalk19-v0",
+     entry_point="gym_custom.envs:GymWalk19",
+     max_episode_steps=300,
+)
--- gym_custom/envs/__init__.py
+++ gym_custom/envs/__init__.py
@@ -1,1 +1,1 @@
-from gym_custom.envs.gym_walk_perso import GymWalk
+from gym_custom.envs.gym_walk_perso import GymWalk,GymWalk19
