08bc95a713c74c2bc6d08204f19dbdbefe421b86
Robin Davenne <robin.davenne@grenoble-inp.org>
2024-10-16 15:30:16 +0900

    changement pour dic

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="gym_custom",
-    version="0.0.4.4.5",
+    version="0.0.4.4.6",
     packages=['gym_custom', 'gym_custom.envs'],
     install_requires=["gymnasium", "pygame"],
 )
